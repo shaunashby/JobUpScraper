98c660252009297ef625d605a4205ef7a9df595d
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-04-22 22:59:31 +0200

    Rename URL test and clean up.
    Replace initial URL test. Rename the test file to conform to Rake glob
    for tests.

--- test/tc_test_url.rb
+++ test/tc_test_url.rb
@@ -4,7 +4,7 @@
 #____________________________________________________________________
 #
 # Author: Shaun Ashby <shaun@ashby.ch>
-# Created: 2014-04-21 15:37:39+0200 (Time-stamp: <2014-04-21 16:07:14 sashby>)
+# Created: 2014-04-21 15:37:39+0200
 # Revision: $Id$
 # Description: Simple test that URL is built correctly.
 #
@@ -12,39 +12,33 @@
 #
 #
 #--------------------------------------------------------------------
-require 'minitest/unit'
+require 'minitest/autorun'
 
-class JobUp
-  class URL
+class URL
+  def initialize
+    @jobup_search_base_url='http://www.jobup.ch/search/joblist.asp'
+    @jobup_cmd='showresults'
+    @jobup_search_subcategories=",147,199,13,194,197,42,71,195,1,72,198,193,88,196,"
+    @jobup_search_cantons="GE1,GE2,GE3,VD1,GE"
+    @jobup_search_keywords="UNIX/Linux/Unix/linux/system%2Badmin/monitoring/puppet/nagios"
+    @jobup_employment_flags="PERMANENT,LIMITED,FREELANCE"
+    @jobup_companytypes="0"
+  end
 
-    def initialize
-      @jobup_search_base_url='http://www.jobup.ch/search/joblist.asp'
-      @jobup_cmd='showresults'
-      @jobup_search_subcategories=",147,199,13,194,197,42,71,195,1,72,198,193,88,196,"
-      @jobup_search_cantons="GE1,GE2,GE3,VD1,GE"
-      @jobup_search_keywords="UNIX/Linux/Unix/linux/system%2Badmin/monitoring/puppet/nagios"
-      @jobup_employment_flags="PERMANENT,LIMITED,FREELANCE"
-      @jobup_companytypes="0"
-    end
-
-    private
-
-    def build_url
-      # FIXME: Use a formatted print to replace this monstrosity...
-      return @jobup_search_base_url+'?cmd='+@jobup_cmd+'&subcategories='+@jobup_search_subcategories+
-        '&cantons='+@jobup_search_cantons+
-        '&keywords='+@jobup_search_keywords+
-        '&employment='+@jobup_employment_flags+
-        '&companytypes='+@jobup_companytypes
-    end
-
+  def build_url
+    # FIXME: Use a formatted print to replace this monstrosity...
+    return @jobup_search_base_url+'?cmd='+@jobup_cmd+'&subcategories='+@jobup_search_subcategories+
+      '&cantons='+@jobup_search_cantons+
+      '&keywords='+@jobup_search_keywords+
+      '&employment='+@jobup_employment_flags+
+      '&companytypes='+@jobup_companytypes
   end
 end
 
-class TestURL < Minitest::Test
+class TestURL < MiniTest::Unit::TestCase
 
   def setup
-    @url = JobUp::URL.new
+    @url = URL.new
   end
 
   def teardown
