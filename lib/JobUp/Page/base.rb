67dea0de5fe38623ddad23eb0de73608b057f5cb
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-04-22 23:01:23 +0200

    Add base class for pages returned from opening a URL.

---
+++ lib/JobUp/Page/base.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+#____________________________________________________________________
+# File: base.rb
+#____________________________________________________________________
+#
+# Author: Shaun Ashby <shaun@ashby.ch>
+# Created: 2014-04-22 22:31:12+0200
+# Revision: $Id$
+# Description: dfd
+#
+# Copyright (C) 2014 Shaun Ashby
+#
+#
+#--------------------------------------------------------------------
+
+module JobUp
+  module Page
+    class Base
+      def initialize
+        @id=nil
+      end
+    end
+  end
+end
