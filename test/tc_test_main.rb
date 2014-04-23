e8495cdec4c7315c56e28aeb9b5efd55f857e9a4
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-04-23 23:20:28 +0200

    Require our module in the test main.

--- test/tc_test_main.rb
+++ test/tc_test_main.rb
@@ -14,6 +14,8 @@
 #--------------------------------------------------------------------
 require 'minitest/autorun'
 
+require 'jobup'
+
 class JobUpMain
   def initialize
     @search_url='http://www.jobup.ch/search/joblist.asp?cmd=showresults&subcategories=,147,199,13,194,197,42,71,195,1,72,198,193,88,196,&cantons=GE1,GE2,GE3,VD1,GE&keywords=UNIX/Linux/Unix/linux/system%2Badmin/monitoring/puppet/nagios&employment=PERMANENT,LIMITED,FREELANCE&companytypes=0'
