5cbf82a76e761a68eaff463ab71fa35ce4dc1b9f
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-04-23 22:34:50 +0200

    Add JobMailer class and placeholder for constants.

--- lib/jobup.rb
+++ lib/jobup.rb
@@ -1,4 +1,24 @@
 module JobUp
   VERSION='0.0.1'
-  
+
+  module JobMailer
+    SUBCATEGORIES=[]
+    CANTONS=[]
+    KEYWORDS=[]
+    EMPLOYMENT=[]
+    COMPANYTYPES={ AGENCIES_ONLY => 0, ENTERPRISE_ONLY => 1,}
+
+    class Criteria
+      def initialize()
+        @options ||= {}
+        @query_string = "a/b/c/d"
+      end
+
+      def build_query_string
+        return @query_string
+      end
+
+    end
+  end
+
 end
