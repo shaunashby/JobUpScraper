9767b5d187cb9dcc8582212935cf9a93ce1f7b56
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-04-23 23:18:18 +0200

    Populate appropriate values for the search criteria constants.

--- lib/jobup.rb
+++ lib/jobup.rb
@@ -2,11 +2,15 @@ module JobUp
   VERSION='0.0.1'
 
   module JobMailer
-    SUBCATEGORIES=[]
-    CANTONS=[]
-    KEYWORDS=[]
-    EMPLOYMENT=[]
-    COMPANYTYPES={ AGENCIES_ONLY => 0, ENTERPRISE_ONLY => 1,}
+
+    SUBCATEGORIES=[ 147, 199, 13, 194, 197, 42, 71, 195, 1, 72, 198, 193, 88, 196 ]
+    CANTONS=[ "GE1", "GE2", "GE3", "VD1", "GE" ]
+    KEYWORDS=[ "UNIX", "Linux", "Unix", "linux", "system+admin", "monitoring", "puppet", "nagios"]
+    EMPLOYMENT=[ "PERMANENT", "LIMITED", "FREELANCE" ]
+
+    COMPANYTYPES_AGENCIES_ONLY = 0
+    COMPANYTYPES_ENTERPRISE_ONLY = 1
+    COMPANYTYPES=[ COMPANYTYPES_AGENCIES_ONLY, COMPANYTYPES_ENTERPRISE_ONLY ]
 
     class Criteria
       def initialize()
