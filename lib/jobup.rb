da26906143098c31f12cd77b2ff9bec57a285981
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-04-24 11:09:44 +0200

    Refactor the Criteria class.

--- lib/jobup.rb
+++ lib/jobup.rb
@@ -5,37 +5,33 @@ module JobUp
 
   module JobMailer
 
-    SUBCATEGORIES=[ 147, 199, 13, 194, 197, 42, 71, 195, 1, 72, 198, 193, 88, 196 ]
-    CANTONS=[ "GE1", "GE2", "GE3", "VD1", "GE" ]
-    KEYWORDS=[ "UNIX", "Linux", "Unix", "linux", "system+admin", "monitoring", "puppet", "nagios"]
-    EMPLOYMENT=[ "PERMANENT", "LIMITED", "FREELANCE" ]
+    SUBCATEGORIES = [ 147, 199, 13, 194, 197, 42, 71, 195, 1, 72, 198, 193, 88, 196 ]
+    CANTONS = [ "GE1", "GE2", "GE3", "VD1", "GE" ]
+    KEYWORDS = [ "UNIX", "Linux", "Unix", "linux", "system+admin", "monitoring", "puppet", "nagios"]
+    EMPLOYMENT = [ "PERMANENT", "LIMITED", "FREELANCE" ]
 
     COMPANYTYPES_ALL = 0
     COMPANYTYPES_ENTERPRISE_ONLY = 1
     COMPANYTYPES_AGENCIES_ONLY = 2
-    COMPANYTYPES=[  COMPANYTYPES_ALL, COMPANYTYPES_AGENCIES_ONLY, COMPANYTYPES_ENTERPRISE_ONLY ]
+    COMPANYTYPES = [  COMPANYTYPES_ALL, COMPANYTYPES_AGENCIES_ONLY, COMPANYTYPES_ENTERPRISE_ONLY ]
 
     class Criteria
       def initialize()
-        @options ||= {}
-        @url=nil
-        @subcategories=SUBCATEGORIES.join(",")
-        @cantons=CANTONS.join(",")
+        @subcategories = SUBCATEGORIES.join(",")
+        @cantons = CANTONS.join(",")
+        @keywords = CGI.escape(KEYWORDS.join("/"))
+        @employment = EMPLOYMENT.join(",")
+
+        @query_uri = sprintf("&subcategories=%s&cantons=%s&keywords=%s&employment=%s&companytypes=%s",
+                             @subcategories,
+                             @cantons,
+                             @keywords,
+                             @employment,
+                             COMPANYTYPES_ALL)
       end
 
-      def query_string
-        return sprintf("&subcategories=%s&cantons=%s",@subcategories,@cantons)
-      end
-
-      def to_url
-        return CGI.escape(@url)
-      end
-
-      private
-
-      def keywords
-        return KEYWORDS.join("/")
-      end
+      attr_reader :query_uri
+      attr_reader :keywords
 
     end
   end
