d4001fac303f8f8d1ed37942ba782d15f17f94fb
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-04-25 23:47:41 +0200

    Add logic to extract the nav block and get pagecount.
    Extract the first nav block from the doc using a search for nav_last CSS class.

--- lib/JobUp/Page/navigator.rb
+++ lib/JobUp/Page/navigator.rb
@@ -16,20 +16,46 @@
 module JobUp
   module Page
 
+    class PageError < Exception; end
+
     class Navigator
 
+      require 'cgi'
+      require 'open-uri'
+
       def initialize(doc)
+        @nav_block
         @pagecount=nil
         @job_uri=Array.new
 
         begin
           if doc.instance_of?(Nokogiri::HTML::Document)
+            # Extract the navigation block:
+            nav_block_nav_last='//*[@id="nav_last"]'
 
+            # Since there will be 2 blocks (for the top and bottom nav bars), we
+            # select just the top one (the first element):
+            @nav_block = doc.xpath(nav_block_nav_last)[0]
+
+            # Check to see if we actually have the nav block. If it's nil then
+            # there were fewer pages so no nav bar wrapping required:
+            if @nav_block.nil?
+              # Proceed to redo the nav block search:
+
+            end
+            # Decode the URI (from the href value):
+            href_str = URI.decode(@nav_block.attribute("href").text)
+
+            # Extract the param components of the URI and find the param "p"
+            # to get the page number. Then convert the string to integer:
+            params=CGI.parse(href_str)
+
+            @pagecount = params['p'][0].to_i
           else
-            raise "Got some other type of document: #{doc.class}"
+            raise PageError, "Got unexpected document type: #{doc.class}"
           end
         rescue => err
-          $stderr.write("ERROR: No Nokogiri document received.")
+          $stderr.print("ERROR: No Nokogiri::HTML::Document object received. Unable to parse.")
         end
       end
 
