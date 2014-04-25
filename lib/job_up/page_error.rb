c58c979e0c211e2b01b99ae19230b169fcdf677e
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-04-26 00:27:18 +0200

    Add generic Page error class.

---
+++ lib/job_up/page_error.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+#____________________________________________________________________
+# File: page_error.rb
+#____________________________________________________________________
+#
+# Author: Shaun Ashby <shaun@ashby.ch>
+# Created: 2014-04-26 00:15:57+0200
+# Revision: $Id$
+# Description: Generic page-handling error class.
+#
+# Copyright (C) 2014 Shaun Ashby
+#
+#
+#--------------------------------------------------------------------
+
+module JobUp
+  class PageError < Exception; end
+end
