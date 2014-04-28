38178d11da2b69be17f5b05037627f889ebafabb
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-04-29 01:09:21 +0200

    Add a test URL for Page::Request test.

--- test/tc_test_page_request.rb
+++ test/tc_test_page_request.rb
@@ -19,6 +19,7 @@ require 'job_up/page/request'
 class TestJobUpPageRequest < MiniTest::Unit::TestCase
 
   TEST_FILE_FOR_URL=File.expand_path(File.dirname(__FILE__) + '/testpage.html')
+  TEST_URL = 'http://www.jobup.ch/search/joblist.asp?cmd=showresults&subcategories=,147,199,13,194,197,42,71,195,1,72,198,193,88,196,&cantons=GE1,GE2,GE3,VD1,GE&keywords=UNIX/Linux/Unix/linux/system%2Badmin/monitoring/puppet/nagios&employment=PERMANENT,LIMITED,FREELANCE&companytypes=0'
 
   def setup
     @base_url = TEST_FILE_FOR_URL
