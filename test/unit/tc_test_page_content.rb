5beb08f6f617e45870d8ad82c6365dd3e03f4524
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-05-01 19:03:37 +0200

    Remove get_nav test for Page::Content.

--- test/unit/tc_test_page_content.rb
+++ test/unit/tc_test_page_content.rb
@@ -35,11 +35,6 @@ class TestJobUpPageContent < MiniTest::Unit::TestCase
     assert_equal("#{@page_content.class}","JobUp::Page::Content")
   end
 
-  def test_page_content_get_nav
-    nav = @page_content.get_nav
-    assert_equal("#{nav.class}","JobUp::Page::Navigator")
-  end
-
   def test_page_content_get_post_count
     assert_equal(@page_content.postcount, 91)
   end
