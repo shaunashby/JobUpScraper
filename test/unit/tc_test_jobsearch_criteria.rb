dc1de6f742a987cbe5760176d3038ee45baebe44
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-05-04 21:02:07 +0200

    Add test for JobSearch::Criteria.

---
+++ test/unit/tc_test_jobsearch_criteria.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+#____________________________________________________________________
+# File: tc_test_jobsearch_criteria.rb
+#____________________________________________________________________
+#
+# Author: Shaun Ashby <shaun@ashby.ch>
+# Created: 2014-05-04 20:37:02+0200
+# Revision: $Id$
+# Description: Test JobSearch::Criteria.
+#
+# Copyright (C) 2014 Shaun Ashby
+#
+#
+#--------------------------------------------------------------------
+require 'minitest/autorun'
+
+require 'job_up'
+
+class TestJobUpSearchCriteria < MiniTest::Unit::TestCase
+
+  SEARCH_TEST_FILE=File.expand_path(File.dirname(__FILE__) + '/../files/test.yml')
+
+  def setup
+    @criteria = JobUp::JobSearch::Criteria.new(354346,
+                                               "System Admin UNIX/Linux",
+                                               :subcategories => [ 147,199,13,194,197,42,71,195,1,72,198,193,88,196 ],
+                                               :cantons       => [ "GE1", "GE2", "GE3", "VD1", "GE" ],
+                                               :keywords      => [ "UNIX","Linux","Unix","linux","system+admin","monitoring","puppet","nagios" ],
+                                               :employment    => [ "PERMANENT","LIMITED","FREELANCE" ],
+                                               :companytypes  => 0)
+  end
+
+  def test_build_query_params
+    assert_equal(@criteria.query_params,
+                 "&subcategories=147,199,13,194,197,42,71,195,1,72,198,193,88,196&cantons=GE1,GE2,GE3,VD1,GE&keywords=UNIX/Linux/Unix/linux/system%2Badmin/monitoring/puppet/nagios&employment=PERMANENT,LIMITED,FREELANCE&companytypes=0")
+  end
+
+end
