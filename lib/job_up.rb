63e617450b10024e91e91017b2b5c43adeee6593
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-05-04 17:35:06 +0200

    Remove obsolete search parameter symbols.

--- lib/job_up.rb
+++ lib/job_up.rb
@@ -17,17 +17,11 @@ module JobUp
   VERSION='0.0.1'
 
   module JobMailer
-
-    SUBCATEGORIES = [ 147, 199, 13, 194, 197, 42, 71, 195, 1, 72, 198, 193, 88, 196 ]
-    CANTONS = [ "GE1", "GE2", "GE3", "VD1", "GE" ]
-    KEYWORDS = [ "UNIX", "Linux", "Unix", "linux", "system+admin", "monitoring", "puppet", "nagios"]
-    EMPLOYMENT = [ "PERMANENT", "LIMITED", "FREELANCE" ]
-
-    COMPANYTYPES_ALL = 0
-    COMPANYTYPES_ENTERPRISE_ONLY = 1
-    COMPANYTYPES_AGENCIES_ONLY = 2
-    COMPANYTYPES = [  COMPANYTYPES_ALL, COMPANYTYPES_AGENCIES_ONLY, COMPANYTYPES_ENTERPRISE_ONLY ]
-
+    #
+    #    COMPANYTYPES_ALL = 0
+    #    COMPANYTYPES_ENTERPRISE_ONLY = 1
+    #    COMPANYTYPES_AGENCIES_ONLY = 2
+    #
     class Criteria
       require 'cgi'
 
