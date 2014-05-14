621c415a28dc38ae647a4e73643736c485e7ec6a
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-05-14 22:51:49 +0200

    Change tests for search: use EN strings, not FR.

--- test/unit/tc_test_search.rb
+++ test/unit/tc_test_search.rb
@@ -18,7 +18,7 @@ require 'job_up/search'
 class TestSearch < MiniTest::Unit::TestCase
 
   def test_search_post_class
-    post = JobUp::Post.new(996104,"1 Mai 2014","IT-Support","› ABC & Cie S.A.","/www/fake/url","› Genève")
+    post = JobUp::Post.new(996104,"1 May 2014","IT-Support","› ABC & Cie S.A.","/www/fake/url","› Geneva")
     assert_instance_of(JobUp::Post, post, "Check instantiation of JobUp::Post")
   end
 
@@ -31,7 +31,7 @@ class TestSearch < MiniTest::Unit::TestCase
 
   def test_search_postcollection_add_post
     post_collection = JobUp::PostCollection.new
-    post = JobUp::Post.new(996104,"1 Mai 2014","IT-Support","› ABC & Cie S.A.","/www/fake/url","› Genève")
+    post = JobUp::Post.new(996104,"1 May 2014","IT-Support","› ABC & Cie S.A.","/www/fake/url","› Geneva")
     post_collection << post
     assert(post_collection.members.length == 1)
   end
