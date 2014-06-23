5507a53e39d2b93db106569bc43c29598d0089be
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-06-23 14:59:54 +0200

    Add spec file to test at application-level.

---
+++ spec/job_up_scraper_spec.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+#____________________________________________________________________
+# File: job_up_scraper_spec.rb
+#____________________________________________________________________
+#
+# Author: Shaun Ashby <shaun@ashby.ch>
+# Created: 2014-06-23 14:56:53+0200
+# Revision: $Id$
+# Description: The main spec for the JobUpScraper application.
+#
+# Copyright (C) 2014 Shaun Ashby
+#
+#
+#--------------------------------------------------------------------
+require File.expand_path '../spec_helper.rb', __FILE__
+
+describe "JobUpScraper application" do
+
+  it "should do something" do
+    pending("not yet implemented")
+    fail
+  end
+
+end
